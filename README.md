dcaa34a1f136e04580f04173879f1767937db2dd
Fabian Martinez <fabianemartinez@gmail.com>
2021-10-20 20:12:47 -0500

    Estructura y componentes del proyecto

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # pokemon-game
 
+Este proyecto consiste en un juego de adivinar el pokemon apartir de su croquis. Para ellos se se utiliza una api que me entrega esta información. El objetivo principal del juego es mostrar como se comunican los componentes de un proyecto ***Vue***.
 ## Project setup
 ```
 npm install
---
+++ src/pages/PokemonPage.vue
@@ -0,0 +1,14 @@
+<template>
+    <h1>¿Quien es este pokemon?</h1>
+    <pokemon-picture/>
+    <pokemon-options/>
+</template>
+
+<script>
+import PokemonPicture from '@/components/PokemonPicture.vue'
+import PokemonOptions from '@/components/PokemonOptions.vue'
+export default {
+  components: { PokemonPicture, PokemonOptions },
+
+}
+</script>
